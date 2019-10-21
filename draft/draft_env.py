d162ed742e0e6d84cf95c271313227541eca4f8a
Ben Glickenhaus <benglickenhaus@gmail.com>
2019-10-21 17:25:32 -0400

    re-worked env so there's a separate state class

--- draft/draft_env.py
+++ draft/draft_env.py
@@ -12,25 +12,57 @@ from dotaservice.protos.dota_shared_enums_pb2 import DOTA_GAMEMODE_ALL_DRAFT
 import uuid
 import pickle
 import docker
+import copy
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 pd.set_option('display.max_rows', 200)
 pd.set_option('display.max_columns', 100)
 
 
-class DraftEnv(ABC):
-    def __init__(self, heros: pd.DataFrame, port):
+# class DraftState:
+#     def __init__(self, draft_state, draft_order, next_pick_index):
+#         self.draft_state = draft_state
+#         self.draft_order = draft_order
+#         self.next_pick_index = next_pick_index
+
+class CaptainModeDraft:
+    def __init__(self, heros: pd.DataFrame):
+        self.next_pick_index = 0
         self.heros = heros
+        self.SEP = heros.loc[heros['name'] == 'SEP', 'model_id'].values[0]
+        self.MASK = heros.loc[heros['name'] == 'MASK', 'model_id'].values[0]
+        self.CLS = heros.loc[heros['name'] == 'CLS', 'model_id'].values[0]
+        self.state = DraftState(np.ones(25) * self.MASK, self.next_pick_index, 13337)
+
+    def reset(self):
+        self.next_pick_index = 0
+        return np.ones(25) * self.MASK
+
+    def step(self, action):
+        next_state, value, done = self.state.take_action(action)
+        self.next_pick_index += 1
+        return next_state, value, done
+
+
+class DraftState(ABC):
+    def __init__(self, state, next_pick_index, port, heros):
         self.port = port
+        self.game_state = state
+        self.next_pick_index = next_pick_index
+
+        self.heros = heros
         self.SEP = heros.loc[heros['name'] == 'SEP', 'model_id'].values[0]
         self.MASK = heros.loc[heros['name'] == 'MASK', 'model_id'].values[0]
         self.CLS = heros.loc[heros['name'] == 'CLS', 'model_id'].values[0]
 
-        self.draft_order = None
+        self.draft_order = np.array([1, 13, 2, 14, 3, 15,
+                                     4, 16, 5, 17,
+                                     6, 18, 7, 19,
+                                     8, 20, 9, 21,
+                                     10, 22,
+                                     11, 23])
 
-        self.initial_state = self.reset()
-        self.next_pick_index = 0
-        self._current_state = self.initial_state
+        self.next_pick_index = next_pick_index
 
         self.TICKS_PER_OBSERVATION = 15
         self.N_DELAY_ENUMS = 5
@@ -38,8 +70,18 @@ class DraftEnv(ABC):
         self.HOST_MODE = HostMode.Value('HOST_MODE_DEDICATED')
 
     @property
+    def player_turn(self):
+        """
+        Returns 1 if Dire, 0 if Radiant
+
+        :return:
+        """
+
+        return self.next_pick_index >= 13
+
+    @property
     def done(self):
-        return -1
+        return self.next_pick_index > 21
 
     @property
     def state(self):
@@ -49,24 +91,15 @@ class DraftEnv(ABC):
 
         :return:
         """
-        return self._current_state
-
-    @property
-    def player_turn(self):
-        """
-        Returns 1 if Dire, 0 if Radiant
-
-        :return:
-        """
-        return -1
+        return self.game_state
 
     @property
     def radiant(self):
-        return self._current_state[[4, 5, 8, 9, 11]]
+        return self.game_state[[4, 5, 8, 9, 11]]
 
     @property
     def dire(self):
-        return self._current_state[[16, 17, 20, 21, 23]]
+        return self.game_state[[16, 17, 20, 21, 23]]
 
     @property
     def radiant_dota_ids(self):
@@ -79,16 +112,25 @@ class DraftEnv(ABC):
     @property
     def get_legal_moves(self):
         return np.array(list((set(self.heros.loc[~self.heros['name'].isin(['MASK', 'SEP', 'CLS']), 'model_id'].values) -
-                              set(self._current_state))))
+                              set(self.game_state))))
 
     def pick(self, hero_id):
-        self._current_state[self.draft_order[self.next_pick_index]] = hero_id
+        self.game_state[self.draft_order[self.next_pick_index]] = hero_id
 
         self.next_pick_index += 1
 
-    def reset(self):
-        self.next_pick_index = 0
-        return np.ones(25) * self.MASK
+    def take_action(self, action):
+        new_state = copy.deepcopy(self.state)
+        new_state[self.draft_order[self.next_pick_index]] = action
+        new_state = DraftState(new_state, self.next_pick_index+1, self.port, self.heros)
+
+        value = 0
+        done = 0
+
+        if new_state.done:
+            value = new_state.get_winner()
+            done = 1
+        return new_state, value, done
 
     def _get_game_config(self):
         radiant_dota_ids = self.heros.loc[self.heros['model_id'].isin(self.radiant), 'id'].values
@@ -113,7 +155,7 @@ class DraftEnv(ABC):
             hero_picks=hero_picks,
         )
 
-    def play(self, config, game_id):
+    def _play(self, config, game_id):
 
         # Reset and obtain the initial observation. This dictates who we are controlling,
         # this is done before the player definition, because there might be humand playing
@@ -148,7 +190,7 @@ class DraftEnv(ABC):
         print(f"Calling play for game id: {game_id}")
         # print(channel_dota.__dict__)
         # try:
-        self.play(config=config, game_id=game_id)
+        self._play(config=config, game_id=game_id)
         # except Exception as e:
         #     print(e)
             # channel_dota.close()
@@ -179,74 +221,74 @@ class DraftEnv(ABC):
         return f'Radiant: {radiant}\nDire: {dire}'
 
 
-class AllPickEnv(DraftEnv):
-    def __init__(self, heros: pd.DataFrame):
-        super().__init__(heros)
-        self.draft_order = np.array([4,
-                            16, 17,
-                            5, 8,
-                            20, 21,
-                            9, 11,
-                            23])
-
-    @property
-    def done(self):
-        return self.next_pick_index >= 10
-
-    @property
-    def player_turn(self):
-        return self.next_pick_index >= 5
-
-    @property
-    def state(self):
-        """
-        Override to mask out the ban indices
-
-        :return:
-        """
-        return np.concatenate([self.CLS],  # Always start with the CLS token - index [0]
-
-                              # Radiant - starts at index 1
-                              np.ones(3) * self.MASK,  # First wave of 3 bans - indices [1, 2, 3]
-                              self._current_state[:2],  # First two picks - indices [4, 5]
-                              np.ones(2) * self.MASK,  # Two more bans - indices [6, 7]
-                              self._current_state[2:4],  # Two more picks - indices [8, 9]
-                              np.ones(1) * self.MASK,  # Final ban - index [10]
-                              self._current_state[4:5],  # Final pick - index [11]
-                              [self.SEP],  # Index [12]
-
-                              # Dire - starts at index 13
-                              np.ones(3) * self.MASK,  # First wave of 3 bans - indices [13, 14, 15]
-                              self._current_state[5:7],  # First two picks - indices [16, 17]
-                              np.ones(2) * self.MASK,  # Two more bans - indices [18, 19]
-                              self._current_state[7:9],  # Two more picks - indices [20, 21]
-                              np.ones(1) * self.MASK,  # Final ban - index [22]
-                              self._current_state[9:],  # Final pick - index [23]
-                              [self.SEP]  # Index [24]
-                              )
-
-
-class CaptainModeEnv(DraftEnv):
-    def __init__(self, heros: pd.DataFrame, port):
-        super().__init__(heros, port)
-        self.draft_order = np.array([1, 13, 2, 14, 3, 15,
-                            4, 16, 5, 17,
-                            6, 18, 7, 19,
-                            8, 20, 9, 21,
-                            10, 22,
-                            11, 23])
-
-    @property
-    def player_turn(self):
-        """
-        Returns 1 if Dire, 0 if Radiant
-
-        :return:
-        """
-
-        return self.next_pick_index >= 13
-
-    @property
-    def done(self):
-        return self.next_pick_index > 21
+# class AllPickState(DraftState):
+#     def __init__(self, heros: pd.DataFrame):
+#         super().__init__(heros)
+#         self.draft_order = np.array([4,
+#                             16, 17,
+#                             5, 8,
+#                             20, 21,
+#                             9, 11,
+#                             23])
+#
+#     @property
+#     def done(self):
+#         return self.next_pick_index >= 10
+#
+#     @property
+#     def player_turn(self):
+#         return self.next_pick_index >= 5
+#
+#     @property
+#     def state(self):
+#         """
+#         Override to mask out the ban indices
+#
+#         :return:
+#         """
+#         return np.concatenate([self.CLS],  # Always start with the CLS token - index [0]
+#
+#                               # Radiant - starts at index 1
+#                               np.ones(3) * self.MASK,  # First wave of 3 bans - indices [1, 2, 3]
+#                               self._current_state[:2],  # First two picks - indices [4, 5]
+#                               np.ones(2) * self.MASK,  # Two more bans - indices [6, 7]
+#                               self._current_state[2:4],  # Two more picks - indices [8, 9]
+#                               np.ones(1) * self.MASK,  # Final ban - index [10]
+#                               self._current_state[4:5],  # Final pick - index [11]
+#                               [self.SEP],  # Index [12]
+#
+#                               # Dire - starts at index 13
+#                               np.ones(3) * self.MASK,  # First wave of 3 bans - indices [13, 14, 15]
+#                               self._current_state[5:7],  # First two picks - indices [16, 17]
+#                               np.ones(2) * self.MASK,  # Two more bans - indices [18, 19]
+#                               self._current_state[7:9],  # Two more picks - indices [20, 21]
+#                               np.ones(1) * self.MASK,  # Final ban - index [22]
+#                               self._current_state[9:],  # Final pick - index [23]
+#                               [self.SEP]  # Index [24]
+#                               )
+#
+#
+# class CaptainModeState(DraftState):
+#     def __init__(self, heros: pd.DataFrame, port):
+#         super().__init__(heros, port)
+#         self.draft_order = np.array([1, 13, 2, 14, 3, 15,
+#                             4, 16, 5, 17,
+#                             6, 18, 7, 19,
+#                             8, 20, 9, 21,
+#                             10, 22,
+#                             11, 23])
+#
+#     @property
+#     def player_turn(self):
+#         """
+#         Returns 1 if Dire, 0 if Radiant
+#
+#         :return:
+#         """
+#
+#         return self.next_pick_index >= 13
+#
+#     @property
+#     def done(self):
+#         return self.next_pick_index > 21
 
