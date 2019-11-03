3413fba149d4982e606b43b0b04e9a16ca4e1c07
Connor Basich <cbasich@cs.umass.edu>
2019-11-03 10:25:35 -0500

    Added new clustering on classes.

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -19,6 +19,40 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, CSS4_COLORS
 
 
+def generate_class_clustering(nodes, algorithm, reduction):
+	nodes = pickle.load( open(nodes, mode = 'rb'), encoding = 'bytes')
+	hero_info = pd.DataFrame(data = json.load( open( os.path.join('..', 'const', 'hero_ids.json'), mode='rb')))
+
+	classes = ["Carry", "Support", "Nuker", "Disabler", "Jungler", "Durable", "Escape", "Pusher", "Initiator"]
+	
+	roles = [[hero_info[hero_info['id'] == id]['roles'].item() for id in node] for node in nodes]
+	# comps = [list(set().union(roles[i][0], roles[i][1], roles[i][2], roles[i][3], roles[i][4])) for i in range(len(roles))]
+	# data = [[int(role in comps[i]) for role in classes] for i in range(len(comps))]
+	comps = [(roles[i][0] + roles[i][1] + roles[i][2] + roles[i][3] + roles[i][4]) for i in range(len(roles))]
+	data = np.array([[comps[i].count(role) for role in classes] for i in range(len(comps))])
+	data /= np.sum(data, axis = 1).reshape(-1,1)
+	X = pd.DataFrame(data, columns = classes)
+	kmeans = KMeans(n_clusters = 8, random_state = 0).fit(X)
+
+	y = kmeans.labels_
+
+	if reduction == 'tsne_2':
+		X_tsne = TSNE(n_components=2, random_state=0).fit_transform(X)
+		x_min, x_max = np.min(X_tsne, 0), np.max(X_tsne, 0)
+		X_tight = (X_tsne - x_min) / (x_max - x_min)
+
+		colors = np.random.choice(list(CSS4_COLORS.keys()), len(np.unique(y)))
+
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		for i in range(len(colors)):
+			tmp = X_tight[y == i]
+			ax.scatter(tmp[:,:1], tmp[:,1:2], c = colors[i])
+
+		plt.xticks([]), plt.yticks([])
+		plt.show()
+
+
 def generate_clustering(embeddings, nodes, algorithm, reduction):
 	embeddings = pickle.load( open(embeddings, mode = 'rb'), encoding = 'bytes')
 	nodes = pickle.load( open(nodes, mode = 'rb'), encoding = 'bytes')
@@ -97,13 +131,14 @@ def generate_clustering(embeddings, nodes, algorithm, reduction):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
-		'--embeddings', '-e', required = True, default= '../data/clustering/10k_embed.pickle')
+		'--embeddings', '-e', required = False, default= '/data/clustering/10k_embed.pickle')
 	parser.add_argument(
-		'--nodes', '-n', required = True, default = '../data/clustering/10k_nodes.pickle')
+		'--nodes', '-n', required = False, default = '/data/clustering/10k_nodes.pickle')
 	parser.add_argument(
 		'--algorithm', '-a', required = False, default = 'kmeans')
 	parser.add_argument(
-		'--reduction', '-r', required = False, default = 'tsne')
+		'--reduction', '-r', required = False, default = 'tsne_2')
 	args = parser.parse_args()
 
-	generate_clustering(args.embeddings, args.nodes, args.algorithm, args.reduction)
+	generate_class_clustering(args.nodes, args.algorithm, args.reduction)
+	# generate_clustering(args.embeddings, args.nodes, args.algorithm, args.reduction)
