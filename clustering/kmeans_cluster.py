9995ed03e6479c4a339d7b9b81fc6c252af5ae39
Connor Basich <cbasich@cs.umass.edu>
2019-11-08 15:52:50 -0500

    Kmeans clustering object now supports creating clusters that do not need to be maintained by the obejct itself. That is the same object can create arbitrarily many cluster on different data, and predict can now take in a cluster which will perform the prediction. Note that there is an object cluster which can be set so that clusters do not need to be specified later.

--- clustering/kmeans_cluster.py
+++ clustering/kmeans_cluster.py
@@ -19,10 +19,14 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, CSS4_COLORS
 
 class kmeans_cluster(object):
-	def __init__(self, centroids):
+	def __init__(self, centroids, train_data):
 		self.classes = ["Carry", "Support", "Nuker", "Disabler", "Jungler", "Durable", "Escape", "Pusher", "Initiator"]
-		hero_info = pd.DataFrame(data = json.load( open( os.path.join('..', 'const', 'hero_ids.json'), mode='rb')))
-		self.cluster = KMeans(n_clusters = centroids, random_state = 0)
+		self.hero_info = pd.DataFrame(data = json.load( open( os.path.join('..', 'const', 'hero_ids.json'), mode='rb')))
+		self.centroids=centroids
+		self.cluster = None
+		if train_data is not None:
+			self.cluster = KMeans(centroids, random_state=0)
+			self.cluster.fit(train_data)
 
 	def process_raw_data(self, input_X):
 		'''
@@ -32,7 +36,7 @@ class kmeans_cluster(object):
 		Returns a pandas datafram object with the correct encoding
 		for clustering. Encoding is based on hero roles in each team. '''
 		nodes = pickle.load(input_X, encoding = 'bytes')
-		roles = [[hero_info[hero_info['id'] == id]['roles'].item() for id in node] for node in nodes]
+		roles = [[self.hero_info[self.hero_info['id'] == id]['roles'].item() for id in node] for node in nodes]
 		comps = [(roles[i][0] + roles[i][1] + roles[i][2] + roles[i][3] + roles[i][4]) for i in range(len(roles))]
 		data = np.array([[comps[i].count(role) for role in self.classes] for i in range(len(comps))])
 		data = data / np.sum(data, axis = 1).reshape(-1,1)
@@ -40,10 +44,29 @@ class kmeans_cluster(object):
 		
 		return X
 
-	def cluster(self, X):
+	def cluster(self, X, centroids=None):
 		X = self.process_raw_data(X)
-		self.cluster.fit(X)
 
-	def predict(self, x):
+		cluster = None
+		if centroids is None:
+			cluster = KMeans(n_clusters=self.centroids, random_state=0)
+		else:
+			clsuter = KMeans(n_clusters=centroids, random_state=0)
+
+		cluster.fit(X)
+		return cluster
+
+
+	def set_cluster(self, cluster):
+		assert cluster is not None
+		self.cluster = cluster
+
+
+	def predict(self, x, cluster=None):
 		x = self.process_raw_data(x)
-		self.cluster.predict(x)
+		y = None
+		if cluster is None:
+			y = self.cluster.predict(x)
+		else:
+			y = cluster.predict(x)
+		return y
