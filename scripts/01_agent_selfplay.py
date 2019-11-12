d8511a0463008a013d6787c3b343c6f9e8793fc8
Ben Glickenhaus <benglickenhaus@gmail.com>
2019-11-11 23:00:53 -0500

    gonna run this self play over night

--- scripts/01_agent_selfplay.py
+++ scripts/01_agent_selfplay.py
@@ -16,7 +16,7 @@ from draft.draft_env import CaptainModeDraft
 from models.draft_agent import DraftAgent, DraftBert
 
 
-def do_rollout(model, hero_ids, num_reads, port, verbose=False):
+def do_rollout(model, hero_ids, num_reads, port, verbose=False, eps=0.1):
     # player_1_pick_first = np.random.choice([True, False])
     player1: DraftAgent = DraftAgent(model=model, pick_first=True)
     player2: DraftAgent = DraftAgent(model=model, pick_first=False)
@@ -44,12 +44,12 @@ def do_rollout(model, hero_ids, num_reads, port, verbose=False):
             raise IndexError
 
         if npi < 13:
-            action, uct_value, p, nn_value, leafs = player1.act(state, action, num_reads=num_reads)
+            action, uct_value, p, nn_value, leafs = player1.act(state, action, num_reads=num_reads, eps=eps)
             player1_nn_values.append(nn_value)
             player1_uct_values.append(uct_value)
             player1_uct_rollout_leafs.append(leafs)
         else:
-            action, uct_value, p, nn_value, leafs = player2.act(state, action, num_reads=num_reads)
+            action, uct_value, p, nn_value, leafs = player2.act(state, action, num_reads=num_reads, eps=eps)
             player2_nn_values.append(nn_value)
             player2_uct_values.append(uct_value)
             player2_uct_rollout_leafs.append(leafs)
@@ -79,34 +79,34 @@ def do_rollout(model, hero_ids, num_reads, port, verbose=False):
                 player2_nn_values=player2_nn_values, player1_uct_values=player1_uct_values, player2_uct_values=player2_uct_values,
                 player1_leafs=player1_uct_rollout_leafs, player2_leafs=player2_uct_rollout_leafs)
 
+
 if __name__ == '__main__':
-    for num_reads in [100, 250, 400, 500]:
-        file_name = None
-        if file_name is None:
-            file_name = f'selfplay_{time.time()}'
-        model: DraftBert = load('../weights/final_weights/draft_bert_pretrain_captains_mode_2.torch',
-                                map_location=device('cpu'))
-        model.eval()
-        model.requires_grad = False
-        memory_size = 500000
-        n_jobs = 4
-        n_games = 100
-        port = 13337
-        verbose = True
-        hero_ids = pd.read_json('../const/draft_bert_hero_ids.json', orient='records')
+    file_name = None
+    if file_name is None:
+        file_name = f'selfplay_{time.time()}'
+    model: DraftBert = load('../weights/final_weights/draft_bert_pretrain_captains_mode_with_clusters.torch',
+                            map_location=device('cpu'))
+    model.eval()
+    model.requires_grad = False
+    memory_size = 500000
+    n_jobs = 4
+    n_games = 500
+    port = 13337
+    verbose = True
+    hero_ids = pd.read_json('../const/draft_bert_hero_ids.json', orient='records')
 
-        memory = deque(maxlen=memory_size)
-        # do_rollout(model, hero_ids, port)
-        f = partial(do_rollout, model, hero_ids, num_reads)
+    memory = deque(maxlen=memory_size)
+    # do_rollout(model, hero_ids, port)
+    f = partial(do_rollout, model, hero_ids, 500)
 
-        for batch_of_games in range(n_games // n_jobs):
-            start = time.time()
+    for batch_of_games in range(n_games // n_jobs):
+        start = time.time()
 
-            start_batch = time.time()
-            with Pool(n_jobs) as pool:
-                results = pool.map_async(f, [port + i for i in range(n_jobs)]).get()
-                memory.extend(results)
-            end = time.time()
-            print(f'Finished batch {batch_of_games} in {end-start}s')
-        with open(f'../data/self_play/{file_name}.pickle', 'wb') as f:
-            pickle.dump(memory, f)
+        start_batch = time.time()
+        with Pool(n_jobs) as pool:
+            results = pool.map_async(f, [port + i for i in range(n_jobs)]).get()
+            memory.extend(results)
+        end = time.time()
+        print(f'Finished batch {batch_of_games} in {end-start}s')
+    with open(f'../data/self_play/{file_name}.pickle', 'wb') as f:
+        pickle.dump(memory, f)
